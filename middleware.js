ddb39e7ed8c307e79f66af3a02db070eff35c57a
XWorkUser <xwork.user1@gmail.com>
2026-03-11 19:46:43 +0400

    changed middileware

--- middleware.js
+++ middleware.js
@@ -6,27 +6,22 @@ const locales = ["az", "en", "ru"];
 export function middleware(request) {
   const { pathname } = request.nextUrl;
 
-  // 1. Redirect: /az -> /
   if (
     pathname === `/${defaultLocale}` ||
     pathname.startsWith(`/${defaultLocale}/`)
   ) {
     const newPath = pathname.replace(`/${defaultLocale}`, "");
-    // Redirect edərkən də parametrləri qorumaq üçün search-i əlavə edirik
     const url = new URL(newPath || "/", request.url);
     url.search = request.nextUrl.search;
     return NextResponse.redirect(url);
   }
 
-  // 2. Rewrite: /xidmetler -> /az/xidmetler
   const pathnameIsMissingLocale = locales.every(
     (locale) =>
       !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
   );
 
   if (pathnameIsMissingLocale) {
-    // BURASI DÜZƏLDİLDİ:
-    // URL-i kopyalayırıq ki, query parametrləri (filter, page və s.) silinməsin
     const url = request.nextUrl.clone();
     url.pathname = `/${defaultLocale}${pathname}`;
 
