771865a4cbc31b06f9db474cd5f8b0e76b8db4c6
XWorkUser <xwork.user1@gmail.com>
2026-03-11 19:45:30 +0400

    instaled tailwindCSS v3, added nextJS config, added middleware.js:

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.azerlegalgroup.com",
+        port: "",
+        pathname: "/storage/**",
+      },
+    ],
+  },
+
+  reactStrictMode: false,
+};
 
 export default nextConfig;
