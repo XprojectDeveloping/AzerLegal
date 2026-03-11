1e392990f722817f20fb23d2a5ef9854a80d0e20
XWorkUser <xwork.user1@gmail.com>
2026-03-11 21:07:30 +0400

    added pages and roots for pages. updated files and configs

---
+++ app/(components)/layout/Header.jsx
@@ -0,0 +1,10 @@
+"use client";
+const Header = () => {
+  return (
+    <header>
+      <h1>Header</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ app/(pages)/[code]/page.jsx
@@ -0,0 +1,13 @@
+import Header from "@/app/(components)/layout/Header";
+import HomePage from "../components/homepage/HomePage";
+import Footer from "@/app/(components)/layout/Footer";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+      <HomePage />
+      <Footer />
+    </main>
+  );
+}
