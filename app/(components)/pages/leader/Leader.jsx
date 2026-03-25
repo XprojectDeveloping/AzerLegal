b3f95f45359a0bfb11f9784f856f4c7455a4dafc
XWorkUser <xwork.user1@gmail.com>
2026-03-25 19:08:24 +0400

    added leader page and styles

--- app/(components)/pages/leader/Leader.jsx
+++ app/(components)/pages/leader/Leader.jsx
@@ -1,5 +1,64 @@
-const Leader = () => {
-  return <div>leader</div>;
+import Image from "next/image";
+import MaxWidth from "../../shared_components/MaxWidth";
+
+const Leader = ({ data, datalang }) => {
+  return (
+    <main>
+      <section className="py-24 bg-[#f8f9fa]">
+        <MaxWidth>
+          <div className="mb-16 text-center">
+            <h2 className="text-[4.8rem] text-[#011E41] font-[700]">
+              {datalang?.leaders_address}
+            </h2>
+          </div>
+
+          <div className=" bg-[#F4F6F6] rounded-[0.8rem] overflow-hidden relative mb-[8rem]">
+            <div className="flex justify-center pt-10">
+              <Image
+                src={`${process.env.NEXT_PUBLIC_PICTURE}/${data?.image}`}
+                alt="Leader"
+                width={420}
+                height={320}
+                className="rounded-2xl shadow-2xl object-cover"
+                priority
+              />
+            </div>
+
+            <div className="flex justify-center -mt-[7rem] mb-[8rem] relative z-10">
+              <Image
+                src="/img/homepage/leader/leader-img.svg"
+                alt="quote"
+                width={180}
+                height={120}
+                className="drop-shadow-sm"
+              />
+            </div>
+
+            <div className="px-12 pb-16 text-center">
+              <div
+                className="text-[2.4rem] font-[700] text-[#011E41]"
+                dangerouslySetInnerHTML={{ __html: data?.title }}
+              />
+            </div>
+          </div>
+
+          <div className="text-center mb-[8rem]">
+            <div
+              className="text-[1.6rem] text-[#011E41] font-[400]"
+              dangerouslySetInnerHTML={{ __html: data?.text1 }}
+            />
+          </div>
+
+          <div className="text-center">
+            <div
+              className="text-[2.4rem] text-[#011E41] font-[600]"
+              dangerouslySetInnerHTML={{ __html: data?.text2 }}
+            />
+          </div>
+        </MaxWidth>
+      </section>
+    </main>
+  );
 };
 
 export default Leader;
--- app/(pages)/[code]/rehberin-muracieti/page.jsx
+++ app/(pages)/[code]/rehberin-muracieti/page.jsx
@@ -1,13 +1,77 @@
 import Footer from "@/app/(components)/layout/Footer";
 import Header from "@/app/(components)/layout/Header";
+import { fetchData, fetchTranslations } from "@/app/(components)/lib/fetchData";
+import {
+  generateKeywordsFromWords,
+  stripHTML,
+} from "@/app/(components)/lib/toSlug";
 import Leader from "@/app/(components)/pages/leader/Leader";
 
-export default function page() {
+const getData = async (code) => {
+  const header = await fetchData(code, "menu");
+  const main = await fetchData(code, "rehberin_muracieti_page");
+  const settings = await fetchData(code, "settings");
+  const translations = await fetchTranslations(code);
+  return { header, main, settings, translations };
+};
+
+export async function generateMetadata({ params }) {
+  try {
+    const { code } = await params;
+    const { settings, main, translations } = await getData(code, "settings");
+    const baseUrl = `${process.env.NEXT_PUBLIC_SITE_NAME}`;
+    const pictureBaseUrl = `${process.env.NEXT_PUBLIC_PICTURE}`;
+    const logoUrl = `${pictureBaseUrl}/${settings?.logo}`;
+    const faviconUrl = `${pictureBaseUrl}/${settings?.favicon}`;
+    const generatedKeyword = generateKeywordsFromWords(
+      main?.rehberin_muracieti?.text1,
+    );
+    const dcs = stripHTML(main?.rehberin_muracieti?.text1);
+
+    return {
+      title: `${settings?.title} - ${translations?.leaders_address}`,
+      description: `${dcs}`,
+      keywords: generatedKeyword,
+      icons: {
+        icon: faviconUrl,
+        apple: faviconUrl,
+      },
+      openGraph: {
+        title: `${settings?.title} - ${translations?.leaders_address}`,
+        description: `${dcs}`,
+        keywords: generatedKeyword,
+        url: `${baseUrl}/${code}/rehberin-muracieti`,
+        siteName: `${baseUrl}/${code}`,
+        type: "website",
+        image: logoUrl,
+        images: [
+          {
+            url: logoUrl,
+            secure_url: logoUrl,
+            width: 100,
+            height: 60,
+            type: "image/png",
+            alt: settings?.title,
+          },
+        ],
+      },
+    };
+  } catch (error) {
+    if (error instanceof Error) {
+      return new Response(error.message, { status: 500 });
+    }
+    return new Response("Internal Server Error", { status: 500 });
+  }
+}
+
+export default async function page({ params }) {
+  const { code } = await params;
+  const { header, main, settings, translations } = await getData(code);
   return (
     <>
-      <Header />
-      <Leader />
-      <Footer />
+      <Header dataHeaderNav={header} code={code} />
+      <Leader data={main?.rehberin_muracieti} datalang={translations} />
+      <Footer data={settings} dataLang={translations} />
     </>
   );
 }
